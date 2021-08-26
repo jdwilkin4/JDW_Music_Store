dbb44782d83a13a8c47a86d58b21d6291ab1f44b
jdwilkin4 <jwilkin4@hotmail.com>
2021-08-25 19:33:46 -0700

    creating unique error message for user signup

--- backend-store/controllers/user.js
+++ backend-store/controllers/user.js
@@ -1,10 +1,15 @@
 const User = require('../models/user');
+const { errorHandler } = require('../helpers/dbHandlerError');
 
 exports.signup = (req, res) => {
     console.log('req.body', req.body)
     const user = new User(req.body)
     user.save((err, user) => {
-        if (err) return console.error(err, 'bad gateway')
+        if (err) {
+            return console.error(errorHandler(err), 'bad gateway')
+        }
+        user.salt = undefined
+        user.hashed_password = undefined
         res.json({ user })
     })
 }
